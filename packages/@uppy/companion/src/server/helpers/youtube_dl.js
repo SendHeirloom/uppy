d8a52e38d0b3d6a62aa2117fb1e6303ed98158f4
Sam Sullivan <samsullivan09@gmail.com>
2023-05-31 01:17:53 -0700

    remove unecessary refactor

--- packages/@uppy/companion/src/server/helpers/youtube_dl.js
+++ packages/@uppy/companion/src/server/helpers/youtube_dl.js
@@ -6,7 +6,7 @@ const TIMEOUT = 30 * 60 * 1000
 function streamFile (url, isAudio, output) {
   return youtubedl.exec(url, {
     output,
-    format: getFormat(isAudio),
+    format: isAudio ? 'bestaudio[ext=m4a]/m4a' : 'worstvideo[height >= 480][ext=mp4]+[ext=m4a]/mp4',
 
     // Fixes 403s, as we are downloading from a different env than this
     noCacheDir: true,
@@ -18,10 +18,6 @@ function streamFile (url, isAudio, output) {
   }, {
     timeout: TIMEOUT,
   })
-}
-
-function getFormat (isAudio) {
-  return isAudio ? 'bestaudio[ext=m4a]/m4a' : 'worstvideo[height >= 480][ext=mp4]+[ext=m4a]/mp4'
 }
 
 module.exports = { streamFile }
