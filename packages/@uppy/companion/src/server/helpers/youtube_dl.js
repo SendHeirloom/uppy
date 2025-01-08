923e35b2f023cd861bcdefa0d1573ff739d05377
Sam Sullivan <samsullivan09@gmail.com>
2025-01-08 08:46:23 -0800

    use cookies file, not proxy

--- packages/@uppy/companion/src/server/helpers/youtube_dl.js
+++ packages/@uppy/companion/src/server/helpers/youtube_dl.js
@@ -17,7 +17,8 @@ function streamFile (url, isAudio, output) {
     noPlaylist: true,
     retries: 1,
 
-    proxy: 'https://sendheirloom_Gc208:NzAXHfgt_JAfh5c@dc.oxylabs.io:8000/',
+    // proxy: 'https://sendheirloom_Gc208:NzAXHfgt_JAfh5c@dc.oxylabs.io:8000/',
+    cookies: '~/uppy/cookies.txt',
   }, {
     timeout: TIMEOUT,
   })
